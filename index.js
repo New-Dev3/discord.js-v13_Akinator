01c72a240c3df411ac8205da065fe13b917212ae
The WarZ <89397215+New-Dev3@users.noreply.github.com>
2022-03-03 22:45:18 +0530

    ⚔️ Akinator Discord Bot ⚔️

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Yo boi!!'));
+app.get('/', (req, res) => res.send('Yo Yeah!!'));
 
 app.listen(port, () =>
 	console.log(`Your app is listening to http://localhost:${port}`)
